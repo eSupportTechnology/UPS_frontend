2b6315b04c48202cd39567436a41fff95112a731
navod <navodthishan@gmail.com>
2025-09-26 14:40:44 +0530

    api fix

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,10 +1,7 @@
 import axios, { AxiosInstance, AxiosError, AxiosResponse } from 'axios';
 
-export const API_BASE_URL = 'http://127.0.0.1:8000';
-export const API_URL = `${API_BASE_URL}/api`;
-
 const api: AxiosInstance = axios.create({
-    baseURL: API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
     timeout: 15000,
     headers: {
         'Content-Type': 'application/json',
